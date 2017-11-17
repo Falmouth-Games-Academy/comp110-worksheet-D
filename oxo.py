ccf49ffbac730d4769ebd604fdec94f430468003
Louis <louis_foy@yahoo.co.uk>
2017-11-17 15:49:16 +0000

    Revision 1

--- oxo.py
+++ oxo.py
@@ -1,26 +1,80 @@
+class OxoTile:
+    EMPTY = 0
+    X = 1
+    O = 2
+
+
 class OxoBoard:
-    def __init__(self):
-        """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+    grid = None  # multidimensional [x][y] list of OxoTile values
+    num_rows = 3  # number of rows on the board
+    num_cols = 3  # number of columns on the board
+    winning_row_length = 3  # number of Xs or Os in a row to win
+
+    def __init__(self, num_rows=3, num_cols=3, winning_row_length=3):
+        """ The initialiser. num_rows and num_cols specify grid size.
+            winning_row_length specifies the length of Xs or Os in a row
+            required to win.
+            """
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.winning_row_length = winning_row_length
+        self.grid = [[0 for y in xrange(self.num_rows)] for x in xrange(self.num_cols)]
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        return self.grid[x][y]
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        if self.grid[x][y] is not OxoTile.EMPTY:
+            return False
+
+        self.grid[x][y] = mark
+        return True
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+        # Loop through all tiles and return False if an empty one is found
+        for x in xrange(0, self.num_cols):
+            for y in xrange(0, self.num_rows):
+                if self.grid[x][y] == OxoTile.EMPTY:
+                    return False
+
+        return True
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+        # Declare a search direction list, representing up-right, right,
+        # down-right and down directions in xy tuples
+        dir_xy = [(1, 0), (1, 1), (1, -1), (0, 1)]
+
+        # Iterate through all items in list
+        for x in xrange(0, self.num_cols):
+            for y in xrange(0, self.num_rows):
+                compare_type = self.grid[x][y]
+
+                if compare_type == OxoTile.EMPTY:
+                    continue
+
+                # Search all directions to see if there's a valid row
+                for dir_id in xrange(0, 4):
+                    for p in xrange(1, self.winning_row_length):
+                        pos_x = x + dir_xy[dir_id][0] * p
+                        pos_y = y + dir_xy[dir_id][1] * p
+
+                        if pos_x >= self.num_cols or pos_y >= self.num_rows or\
+                            pos_x < 0 or pos_y < 0:
+                            break
+
+                        if self.grid[pos_x][pos_y] is not compare_type:
+                            break
+                        if p == self.winning_row_length - 1:
+                            return compare_type  # Winner!
+
+        return 0
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
