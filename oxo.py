77bd94f161ea19e65744885755f5b5fa6c68244a
Tomas Mazurkevix <mazurkevic.tomas@gmail.com>
2017-12-01 12:01:28 +0000

    * Changes

--- oxo.py
+++ oxo.py
@@ -1,26 +1,46 @@
 class OxoBoard:
+    # empty = 0
+    # player_X = 1
+    # player_O = 2
+
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        self.tile_width = 3
+        self.tile_height = 3
+        self.tile = [[0 for y in range(self.tile_width)] for x in xrange(self.tile_height)]
+        print self.tile
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        return self.tile[x][y]
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        if self.tile[x][y] == 0:
+            self.tile[x][y] = mark
+            return True
+        else:
+            return False
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+        for x in xrange(self.tile_width):
+            for y in xrange(self.tile_height):
+                if self.tile[x][y] == 0:
+                    return False
+                else:
+                    return True
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+        for x in xrange(self.tile_width):
+            for y in xrange(self.tile_height):
+                if self.tile
+
+        return 0
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
