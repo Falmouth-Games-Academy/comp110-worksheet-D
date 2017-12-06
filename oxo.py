aecd3c9666a7ff42caa4b45afc33e80c47b62cd3
ac200905 <ac200905@falmouth.ac.uk>
2017-12-06 13:18:23 +0000

    First basic build attempt.

--- oxo.py
+++ oxo.py
@@ -1,26 +1,57 @@
 class OxoBoard:
+
+    board_width = 3
+    board_height = 3
+
+
     def __init__(self):
-        """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        self.matrix = [[0,0,0],[0,0,0],[0,0,0]]
 
     def get_square(self, x, y):
-        """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+
+        return self.matrix[x][y]
 
     def set_square(self, x, y, mark):
-        """ If the specified square is currently empty (0), fill it with mark and return True.
-            If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+
+        if self.matrix[x][y] == 0:
+            self.matrix[x][y] = mark
+            return True
+        else:
+            return False
 
     def is_board_full(self):
-        """ If there are still empty squares on the board, return False.
-            If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+
+        for x in xrange(0, self.board_width):
+            for y in xrange(0, self.board_height):
+                if self.matrix[x][y] == 0:
+                    return False
+        return True
 
     def get_winner(self):
-        """ If a player has three in a row, return 1 or 2 depending on which player.
-            Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+
+        if (self.matrix[0][0] == self.matrix[1][0] == self.matrix[2][0] == 1) or \
+                (self.matrix[0][1] == self.matrix[1][1] == self.matrix[2][1] == 1) or \
+                (self.matrix[0][2] == self.matrix[1][2] == self.matrix[2][2] == 1) or \
+                (self.matrix[0][0] == self.matrix[0][1] == self.matrix[0][2] == 1) or \
+                (self.matrix[1][0] == self.matrix[1][1] == self.matrix[1][2] == 1) or \
+                (self.matrix[2][0] == self.matrix[2][1] == self.matrix[2][2] == 1) or \
+                (self.matrix[0][0] == self.matrix[1][1] == self.matrix[2][2] == 1) or \
+                (self.matrix[0][2] == self.matrix[1][1] == self.matrix[2][0] == 1):
+            return 1
+
+        if (self.matrix[0][0] == self.matrix[1][0] == self.matrix[2][0] == 2) or \
+                (self.matrix[0][1] == self.matrix[1][1] == self.matrix[2][1] == 2) or \
+                (self.matrix[0][2] == self.matrix[1][2] == self.matrix[2][2] == 2) or \
+                (self.matrix[0][0] == self.matrix[0][1] == self.matrix[0][2] == 2) or \
+                (self.matrix[1][0] == self.matrix[1][1] == self.matrix[1][2] == 2) or \
+                (self.matrix[2][0] == self.matrix[2][1] == self.matrix[2][2] == 2) or \
+                (self.matrix[0][0] == self.matrix[1][1] == self.matrix[2][2] == 2) or \
+                (self.matrix[0][2] == self.matrix[1][1] == self.matrix[2][0] == 2):
+            return 2
+
+        else:
+            return 0
+
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
