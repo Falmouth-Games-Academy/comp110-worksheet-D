cf44e6de76b76f4d5ea3014c48f3f4317b6cf48c
Tomas Mazurkevic <mazurkevic.tomas@gmail.com>
2017-12-07 17:55:55 +0200

    * Implemented get_winner and it is working

--- oxo.py
+++ oxo.py
@@ -1,14 +1,12 @@
 class OxoBoard:
-    # empty = 0
-    # player_X = 1
-    # player_O = 2
+    empty = 0
 
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
         self.tile_width = 3
         self.tile_height = 3
-        self.tile = [[0 for y in range(self.tile_width)] for x in xrange(self.tile_height)]
-        print self.tile
+        self.tile = [[0 for y in range(self.tile_width)]
+                     for x in xrange(self.tile_height)]
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
@@ -17,7 +15,7 @@ class OxoBoard:
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        if self.tile[x][y] == 0:
+        if self.tile[x][y] == self.empty:
             self.tile[x][y] = mark
             return True
         else:
@@ -28,17 +26,43 @@ class OxoBoard:
             If there are no empty squares, return True. """
         for x in xrange(self.tile_width):
             for y in xrange(self.tile_height):
-                if self.tile[x][y] == 0:
+                if self.tile[x][y] == self.empty:
                     return False
-                else:
-                    return True
+
+        return True
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        for x in xrange(self.tile_width):
-            for y in xrange(self.tile_height):
-                if self.tile
+        for i in xrange(3):
+            for j in xrange(3):
+                compare = self.tile[i][j]
+
+                if compare == self.empty:
+                    continue
+
+                for vector in [(1, 0), (1, 1), (0, 1), (-1, 1)]:
+                    try:
+                        box_to_check = [i, j]
+                        char_to_check_for = self.tile[i][j]
+                        for x in range(1, 3):
+                            box_to_check[0] += vector[0]
+                            box_to_check[1] += vector[1]
+
+                            if self.tile[box_to_check[0]][box_to_check[1]] != char_to_check_for:
+                                break
+
+                            if box_to_check[0] >= self.tile_width or\
+                                    box_to_check[1] >= self.tile_height or\
+                                    box_to_check[0] < 0 or\
+                                    box_to_check[1] < 0:
+                                break
+
+                            if x == 2:
+                                return compare
+
+                    except:
+                        continue
 
         return 0
 
