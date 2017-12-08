cbc3b1f60f6ab2d7fbab3250dc3011896f7dd169
Beren Wallis <32566086+BerenWallis@users.noreply.github.com>
2017-12-08 13:08:46 +0000

    Added NotImplementedError

--- oxo.py
+++ oxo.py
@@ -1,6 +1,7 @@
 class OxoBoard:
     loc = []
     def __init__(self):
+        raise NotImplementedError("TODO: implement __init__")
         for i in range(0,3):
             temp = []
             for j in range(0,3):
@@ -8,9 +9,11 @@ class OxoBoard:
             self.loc.append(temp)
 
     def get_square(self, x, y):
+        raise NotImplementedError("TODO: implement get_square")
         return self.loc[x][y]
 
     def set_square(self, x, y, mark):
+        raise NotImplementedError("TODO: implement set_square")
         if self.loc[x][y] == 0:
             self.loc[x][y]=mark
             return True
@@ -18,6 +21,7 @@ class OxoBoard:
             return False
 
     def is_board_full(self):
+        raise NotImplementedError("TODO: implement is_board_full")
         for x in range(0,3):
             for y in range(0,3):
                 if self.loc[x][y] == 0:
@@ -26,6 +30,7 @@ class OxoBoard:
 
 
     def get_winner(self):
+        raise NotImplementedError("TODO: implement get_winner")
         for x in range(0,3):
             for win in range(1,3):
                 if self.loc[x][0] == win and self.loc[x][1] == win and self.loc[x][2] == win:
