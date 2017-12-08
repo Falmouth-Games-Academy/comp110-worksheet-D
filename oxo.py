76ffd2fc27d2f014106b72ad649d3290d96ace90
Beren Wallis <32566086+BerenWallis@users.noreply.github.com>
2017-12-08 13:44:00 +0000

    Added Doc Strings and descriptions

--- oxo.py
+++ oxo.py
@@ -9,9 +9,11 @@ class OxoBoard:
             self.loc.append(temp)
 
     def get_square(self, x, y):
+        """Returns Value Of Square"""
         return self.loc[x][y]
 
     def set_square(self, x, y, mark):
+        """Sets Value Of A Square"""
         if self.loc[x][y] == 0:
             self.loc[x][y]=mark
             return True
@@ -19,6 +21,7 @@ class OxoBoard:
             return False
 
     def is_board_full(self):
+        """Checks If The Board Is Full, Returns True If Board Is Full"""
         for x in range(0,3):
             for y in range(0,3):
                 if self.loc[x][y] == 0:
@@ -27,6 +30,7 @@ class OxoBoard:
 
 
     def get_winner(self):
+        """ Checks If there is a winner in the game returns 0 if no winner or 1/2 depending on winner"""
         for x in range(0,3):  # checks vertical
             for win in range(1,3):
                 if self.loc[x][0] == win and self.loc[x][1] == win and self.loc[x][2] == win:
