15278cdda9d926390b24e7b8218f05748f491b86
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-11-20 13:34:15 +0000

    Added visual X&Os

--- oxo.py
+++ oxo.py
@@ -1,26 +1,36 @@
 class OxoBoard:
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        # nested list of square contents
+        self.square = [[0, 0, 0],
+                       [0, 0, 0],
+                       [0, 0, 0]]
 
     def get_square(self, x, y):
-        """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        """ Return 0, 1 or 2 depending on the contents of the specified square."""
+        return self.square[y][x]
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+        if self.square[y][x] == 0:
+            self.square[y][x] = mark
+            return True
+        else:
+            return False
+        # raise NotImplementedError("TODO: implement set_square")
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
+        pass
+        # raise NotImplementedError("TODO: implement is_board_full")
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+        return 0
+        # raise NotImplementedError("TODO: implement get_winner")
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
