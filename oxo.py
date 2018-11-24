6141eafa83c13fba8e2e1f05005157d9cb12b135
Josh Grillo <Josh@Grillo.co.uk>
2018-11-24 15:32:02 +0000

    Added comments about how I would improve to allow different sized games, if we were allowed to change play.py

--- oxo.py
+++ oxo.py
@@ -1,12 +1,25 @@
 class OxoBoard:
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        self.board = [[0 for x in range(3)] for y in range(3)]
+        self.size = 3
+        self.board = [[0 for x in range(self.size)] for y in range(self.size)]
         '''
          x = 1,2,3
            [[a,b,c], y = 0
             [d,e,f], y = 1
             [g,h,i]] y = 2      i think
+            
+            it'd be pretty easy to implement bigger boards for this, need:
+            -> a screen that asks players how big they want the board
+            -> this __init__ to have an extra parameter for board size
+                -> this parameter also determines grid_width/height in play.py,
+                 the screen size would stay the same, but the boxes are smaller
+            -> the new parameter would be assigned to self.size
+            -> searches for non-diagonal wins would have to be updated, as they
+                are currently hard coded because it was nice and easy :(
+                
+            pep-8 is lame so I didn't stick too strictly to it,
+            comments are sparse because doc-strings exist :)
         '''
 
     def get_square(self, x, y):
@@ -24,10 +37,9 @@ class OxoBoard:
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        for x in range(3):
-            for y in range(3):
-                if self.board[y][x] == 0:
-                    return False
+        for x in range(self.size):
+            for y in range(self.size):
+                if self.board[y][x] == 0: return False
         return True
 
     def get_winner(self):
@@ -46,23 +58,21 @@ class OxoBoard:
                     self.board[0][x] == self.board[1][x] == self.board[2][x]:
                 return self.board[0][x]
 
-        # -ve Diagonal
-
-        for diag in range(0,3,2):
+        # Diagonals
+        for diagonal in range(0,self.size,self.size-1):
+            # first iteration looks and -ve diagonal, second at +ve diagonal
             mark = None
             possible = True
             while possible:
-                for y in range(3):
-                    x = abs(diag - y)
-                    if mark == None:
+                for y in range(self.size):
+                    x = abs(diagonal - y)
+                    if mark is None:
                         mark = self.board[y][x]
                     else:
                         if mark != self.board[y][x]:
                             possible = False
-                if possible:
-                    return mark
+                if possible: return mark
         return 0
-
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
