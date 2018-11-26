3b5838551cdb11ff4c1390d5f633d5d1bcf36fc6
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-11-26 09:02:17 +0000

    Finished scruffy get_winner. Also removed code from stack overflow (NOT_MY_CODE.py)

--- oxo.py
+++ oxo.py
@@ -1,18 +1,37 @@
 class OxoBoard:
+
     def __init__(self):
         """ The initialiser. Initialise any fields you need here."""
+
         # nested list of square contents
         self.squares = [[0, 0, 0],
                         [0, 0, 0],
                         [0, 0, 0]]
 
     def get_square(self, x, y):
-        """ Return 0, 1 or 2 depending on the contents of the specified square."""
+        """
+        _______________________________________________________________________
+        Return 0, 1 or 2 depending on the contents of the specified square.
+        :param x: grid x position
+        :param y: grid y position
+        :return: current contents of grid at specified position
+        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+        """
+
         return self.squares[y][x]
 
     def set_square(self, x, y, mark):
-        """ If the specified square is currently empty (0), fill it with mark and return True.
-            If the square is not empty, leave it as-is and return False. """
+        """
+        _______________________________________________________________________
+         If the specified square is currently empty (0), fill it with mark and
+         return True. Otherwise , leave it as-is and return False.
+        :param x: grid x position
+        :param y: grid y position
+        :param mark: current grid position's contents (0, 1, or 2)
+        :return: True or False depending on whether square is full
+        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+        """
+
         if self.squares[y][x] == 0:
             self.squares[y][x] = mark
             return True
@@ -20,51 +39,53 @@ class OxoBoard:
             return False
 
     def is_board_full(self):
-        """ If there are still empty squares on the board, return False.
-            If there are no empty squares, return True."""
-        if 0 in [elem for sublist in self.squares for elem in sublist]:
-            print('not full')
+        """
+        _______________________________________________________________________
+        If there are still empty squares on the board, return False.
+            If there are no empty squares, return True.
+        :return: True or False depending on whether all squares are full
+        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+        """
+
+        if 0 in [items for sublist in self.squares for items in sublist]:
             return False
 
         else:
-            print('full')
             return True
 
     def get_winner(self, player):
-        """ If a player has three in a row, return 1 or 2 depending on which player.
-            Otherwise, return 0."""
-        '''
-        for i in range(0, len(self.squares)):  # check vertically
-            if self.squares[0][i] == \
-                    self.squares[0][i] == \
-                    self.squares[0][i] and self.squares[0][i] != 0:
-                print('Vertical win')
-                return player
-        for i in range(0, len(self.squares)):  # check vertically
+        """
+        _______________________________________________________________________
+        If a player has three in a row, return 1 or 2 depending on which
+        player. Otherwise, return 0.
+        :param player: current player (1 or 2)
+        :return: winning player (0 if no win this turn)
+        ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+        """
+
+        for i in range(0, len(self.squares)):
+            # check horizontal win
             if self.squares[i][0] == \
                     self.squares[i][1] == \
-                    self.squares[i][2] and self.squares[i][0] != 0:
-                print('Horizontal win')
+                    self.squares[i][2] != 0:
                 return player
-        print('none')
-        return 0
-        '''
-
-        for i in range(0, len(self.squares)):  # check vertically
-            if self.squares[0][i] == \
+            # check vertical win
+            elif self.squares[0][i] == \
                     self.squares[1][i] == \
                     self.squares[2][i] != 0:
-                print('Vertical win')
                 return player
-            elif self.squares[i][0] == \
-                    self.squares[i][1] == \
-                    self.squares[i][2] != 0:
-                print('Horizontal win')
+            # check diagonal win
+            elif self.squares[0][i-2] == \
+                    self.squares[1][i-1] == \
+                    self.squares[2][i] != 0:
                 return player
-            else:
-                return 0
+            # check diagonal win
+            elif self.squares[0][i] == \
+                    self.squares[1][i-1] == \
+                    self.squares[2][i-2] != 0:
+                return player
 
-            # TODO: implement diagonal win
+        return 0
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this."""
